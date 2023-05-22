277eba81d1a51709b5ab642945f606ca1b3368d8
kiet019 <kietly019@gmail.com>
2023-05-22 13:53:26 +0700

    update

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -4,19 +4,23 @@ import Layout from "../components/Layout";
 export default function news() {
   useEffect(() => {
     const getData = async () => {
-      const response = await fetch("http://localhost:4000", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          query: `query Query {
-                    getNews(ids : []) { id img title description by }
-                  }
-                  `,
-      })})
-      const data = await response.json();
-      console.log(data.data.getNews);
+        try {
+        const response = await fetch("http://localhost:4000", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            query: `query Query {
+                      getNews(ids : []) { id img title description by }
+                    }
+                    `,
+        })})
+        const data = await response.json();
+        console.log(data.data.getNews);
+      } catch(error) {
+        console.log(error)
+      }
     }
     getData();
   }, []);
